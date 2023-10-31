98b5995396a691440c64c96f480cba776b8d7beb
jorgeholberton <ingesoft99@gmail.com>
2023-10-30 19:01:05 -0500

    Parte2

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Recipe } from '../../recipe.model';
 
 @Component({
@@ -12,5 +12,19 @@ export class RecipeItemComponent
 	/* @Input decorador para poder obtener el valor desde afuera */
   @Input()
   recipe!: Recipe;/* No se le asigna valor pues se obtiene desde el parentcomponent, con ! se evita
-                  que se marque como error si no tiene un valor definido */
+  que se marque como error si no tiene un valor definido */
+	
+  /* El emisor tendra void pues no pasa información
+  con @Output para escucharlo desde afuera, desde el parentcomponent*/
+  @Output() recipeSelected = new EventEmitter<void>();
+ 
+  constructor() { }
+                   
+  ngOnInit() {}
+                   
+  onSelected() 
+  {
+    this.recipeSelected.emit(); /* Se emite el evento */
+  }
+
 }
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,23 +7,28 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.scss']
 })
 
-export class RecipeListComponent implements OnInit {
+export class RecipeListComponent implements OnInit 
+{
  
   /* arreglo del tipo Modelo Recipe donde se encuentran los elementos de la receta */
-  recipes: Recipe[] = 
-  [
-    new Recipe('Paella valenciana',
-      'Recipe description',
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/e/ed/01_Paella_Valenciana_original.jpg/800px-01_Paella_Valenciana_original.jpg'),
-      
-      new Recipe('Recipe title test',
-      'Recipe description',
-      'https://p1.pxfuel.com/preview/683/172/968/cake-sponge-cake-bowl-cake-small.jpg')
-    ];
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+recipes: Recipe[] = [
+  new Recipe('Paella valenciana',
+    'Recipe description',
+    'https://upload.wikimedia.org/wikipedia/commons/thumb/e/ed/01_Paella_Valenciana_original.jpg/800px-01_Paella_Valenciana_original.jpg'),
+  new Recipe('Recipe title test',
+    'Recipe description',
+    'https://p1.pxfuel.com/preview/683/172/968/cake-sponge-cake-bowl-cake-small.jpg')
+];
  
-  constructor() { }
+constructor() { }
  
-  ngOnInit() {
-  }
+ngOnInit() {
+}
+ 
+onRecipeSelected(recipe: Recipe) 
+{
+  this.recipeWasSelected.emit(recipe);
+}
  
 }
